6e5252a79b39552a3f7197388b7e0c08e57f188f
Joakim Bording <joakim@netliferesearch.com>
2015-01-29 19:37:17 +0100

    First version

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # statamic-delete-content
 A Statamic add-on that enables you to delete content files by a post request. You could for instance let members delete entries, or content created by the Reven add-on, without going into the admin interface. 
+
+Example markup
+```html
+{{ entries:listing folder="blogs" }}
+{{ title }}
+<form action="" method="post">
+	<input type="hidden" name="delete-file" value="{{ url }}"/>					
+	<button type="submit">Delete</button>
+</form>	
+{{ /entries:listing }}	
+```
---
+++ _add-ons/delete-content/hooks.delete-content.php
@@ -0,0 +1,45 @@
+<?php 
+	
+class Hooks_delete-content extends Hooks {
+
+	var $meta = array(
+		'name'       => 'Delete Content - makes it possible to delete contentfiles',
+		'version'    => '1.0',
+		'author'     => 'Joakim Bording',
+		'author_url' => 'http://joakim.bording.no/'
+	);
+	
+
+	public function request__post()
+	{
+
+		if (array_get($_POST, 'delete-content')) {
+			$filename = array_get($_POST, 'delete-content');	
+
+			// Remove subdirectory
+			$array = explode("/", $filename);
+			unset($array[0]);
+			unset($array[1]);			
+			$filename = '/'.implode("/", $array);
+			
+			// Ad in order elements				
+			$filename = Path::resolve($filename);			
+			
+			// Construct the full url
+			$filename = Config::getSiteRoot().'_content'.$filename.'.html';
+			
+			// Add below webroot
+			$filename = Path::fromAsset($filename);
+
+			// Delete file
+			FILE::delete($filename);
+			
+			// Redirect to avoid refresh problems
+			$currenturl = URL::getCurrent();			
+			URL::redirect($currenturl);
+		}
+	}
+
+}
+
+?>
